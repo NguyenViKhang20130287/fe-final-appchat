138549dda3dbf610fcea3b2fb7127fbb9c429b2a
unknown <20130262@st.hcmuaf.edu.vn>
2023-06-15 18:34:56 +0700

    validation and mount api for login & register

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Login_content from "../components/login/Login_content";
+import Login_password_option from "../components/login/Login_password_option";
+import Login_register from "../components/login/Login_register";
 
 
 export default function Login() {
   return <div className="login_site">
     <Login_content />
+
   </div>;
 }
